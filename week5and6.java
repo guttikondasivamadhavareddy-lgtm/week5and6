54d6a7749e56902398c3d48d99f45d4b62271129
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 19:48:45 +0530

    level 1 question 1 completed

--- week5and6.java
+++ week5and6.java
@@ -4,21 +4,34 @@ public class week5and6 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int[] ages = new int[10];
+        int[] arr = new int[5];
 
-        for (int i = 0; i < ages.length; i++) {
-            System.out.print("Enter age of student " + (i + 1) + ": ");
-            ages[i] = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < ages.length; i++) {
-            if (ages[i] < 0) {
-                System.out.println("Invalid age: " + ages[i]);
-            } else if (ages[i] >= 18) {
-                System.out.println("The student with age " + ages[i] + " can vote.");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                if (arr[i] % 2 == 0) {
+                    System.out.println(arr[i] + " is Positive and Even");
+                } else {
+                    System.out.println(arr[i] + " is Positive and Odd");
+                }
+            } else if (arr[i] < 0) {
+                System.out.println(arr[i] + " is Negative");
             } else {
-                System.out.println("The student with age " + ages[i] + " cannot vote.");
+                System.out.println(arr[i] + " is Zero");
             }
+        }
+
+        // Compare first and last
+        if (arr[0] == arr[4]) {
+            System.out.println("First and last elements are equal");
+        } else if (arr[0] > arr[4]) {
+            System.out.println("First element is greater than last");
+        } else {
+            System.out.println("First element is less than last");
         }
     }
 }
