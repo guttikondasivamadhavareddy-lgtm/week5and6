5964554c810cd90a206570bcfb5f59c542df3134
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 20:07:26 +0530

    level3 question 1 completed

--- week5and6.java
+++ week5and6.java
@@ -5,32 +5,42 @@ public class week5and6 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number: ");
+        System.out.print("Enter a number: ");
         int number = sc.nextInt();
 
-        int maxDigit = 10;
-        int[] digits = new int[maxDigit];
+        int temp = number;
+        int count = 0;
+
+        // Step 1: Count digits
+        while (temp != 0) {
+            count++;
+            temp = temp / 10;
+        }
+
+        // Step 2: Store digits in array
+        int[] digits = new int[count];
+        temp = number;
         int index = 0;
 
-        // Extract digits
-        while (number != 0 && index < maxDigit) {
-            digits[index] = number % 10;
-            number = number / 10;
-            index++;
+        while (temp != 0) {
+            digits[index++] = temp % 10;
+            temp = temp / 10;
         }
 
-        int largest = 0, secondLargest = 0;
+        // Step 3: Frequency array (0–9)
+        int[] freq = new int[10];
 
-        for (int i = 0; i < index; i++) {
-            if (digits[i] > largest) {
-                secondLargest = largest;
-                largest = digits[i];
-            } else if (digits[i] > secondLargest && digits[i] != largest) {
-                secondLargest = digits[i];
+        for (int i = 0; i < count; i++) {
+            freq[digits[i]]++;
+        }
+
+        // Step 4: Display result
+        System.out.println("\nDigit Frequencies:");
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + freq[i] + " times");
             }
         }
-
-        System.out.println("Largest digit: " + largest);
-        System.out.println("Second largest digit: " + secondLargest);
     }
 }
